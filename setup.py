88e21de42279568d9277a798a4ab2a355855c4dd
Derek Stegelman <dstegelman@gmail.com>
2013-05-11 21:51:13 -0600

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+version = '0.1'
+
+setup(name='django-locations',
+      version=version,
+      description="A simple locations app for Django",
+      long_description=open("README.md", "r").read(),
+      classifiers=[
+          "Development Status :: 5 - Production/Stable",
+          "Environment :: Web Environment",
+          "Intended Audience :: End Users/Desktop",
+          "Natural Language :: English",
+          "Operating System :: OS Independent",
+          "Framework :: Django",
+          "Programming Language :: Python",
+          "Topic :: Internet :: WWW/HTTP :: Dynamic Content :: CGI Tools/Libraries",
+          "Topic :: Utilities",
+          "License :: OSI Approved :: MIT License",
+          ],
+      keywords='',
+      author='Derek Stegelman',
+      author_email='dstegelman@gmail.com',
+      url='http://github.com/dstegelman/django-locations',
+      license='MIT',
+      packages=find_packages(),
+      install_requires = [],
+      include_package_data=True,
+      zip_safe=False,
+    )
