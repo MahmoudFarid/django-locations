e4b30765b13101601d6551259d1e69c2df88663c
Derek Stegelman <dstegelman@gmail.com>
2015-03-30 20:41:33 -0600

    bupm with wheels make python 2 compat.

--- locations/models.py
+++ locations/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
 
 from geopy.geocoders import GoogleV3
 
@@ -6,6 +7,7 @@ from locations.choices import STATE_CHOICES, COUNTRY_CHOICES
 from locations.utils import unique_slugify
 
 
+@python_2_unicode_compatible
 class Location(models.Model):
     city = models.CharField(max_length=250)
     state = models.CharField(max_length=250, choices=STATE_CHOICES, blank=True, help_text="State is not required if you are out of the country.")
